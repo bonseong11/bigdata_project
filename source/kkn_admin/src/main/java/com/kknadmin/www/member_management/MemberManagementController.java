11583c5dbd37b7f06099aab143e96657e15fc9f3
ren <131874337+Ren-Sato-1212@users.noreply.github.com>
2023-07-10 10:24:05 +0900

    page bug modified

--- source/kkn_admin/src/main/java/com/kknadmin/www/member_management/MemberManagementController.java
+++ source/kkn_admin/src/main/java/com/kknadmin/www/member_management/MemberManagementController.java
@@ -8,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.kknadmin.www.member_management.dto.MemberListDto;
+
 @Controller
 @RequestMapping("/member")
 public class MemberManagementController {
--- source/kkn_admin/src/main/java/com/kknadmin/www/member_management/repository/MemberListDtoManagementRepository.java
+++ source/kkn_admin/src/main/java/com/kknadmin/www/member_management/repository/MemberListDtoManagementRepository.java
@@ -1,6 +1,8 @@
-package com.kknadmin.www.member_management;
+package com.kknadmin.www.member_management.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.kknadmin.www.member_management.dto.MemberListDto;
 
 public interface MemberListDtoManagementRepository extends JpaRepository<MemberListDto, String> {
 }
