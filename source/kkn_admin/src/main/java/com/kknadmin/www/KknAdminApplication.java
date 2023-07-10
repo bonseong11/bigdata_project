1bf16d33d34dbc86b01938f8ba940a9a7e99f2d5
ren <131874337+Ren-Sato-1212@users.noreply.github.com>
2023-07-10 10:25:59 +0900

    page bug modified

--- source/kkn_admin/src/main/java/com/kknadmin/www/KknAdminApplication.java
+++ source/kkn_admin/src/main/java/com/kknadmin/www/KknAdminApplication.java
@@ -2,15 +2,11 @@ package com.kknadmin.www;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class KknAdminApplication {
 	
 	public static void main(String[] args) {
-		BCryptPasswordEncoder bps = new BCryptPasswordEncoder();
-		
-		System.out.println(bps.encode("dbsgh123"));
 		SpringApplication.run(KknAdminApplication.class, args);
 	}
 
