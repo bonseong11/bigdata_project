b893ffc495c26e508cff3bd0961d1524842978f1
佐藤蓮 <131874337+Ren-Sato-1212@users.noreply.github.com>
2023-07-11 09:18:23 +0900

    Delete source/kkn_admin/src/main/java/com/kknadmin/www/member_management/repository directory

--- source/kkn_admin/src/main/java/com/kknadmin/www/member_management/repository/MemberListDtoManagementRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kknadmin.www.member_management.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kknadmin.www.member_management.dto.MemberListDto;
-
-public interface MemberListDtoManagementRepository extends JpaRepository<MemberListDto, String> {
-}
--- source/kkn_admin/src/main/java/com/kknadmin/www/member_management/repository/MemberManagementRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kknadmin.www.member_management.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kknadmin.www.entity.Member;
-
-public interface MemberManagementRepository extends JpaRepository<Member, String> {
-}
