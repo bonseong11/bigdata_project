37ce633fcff141ab9bd55b5848f89af316ff98f4
佐藤蓮 <131874337+Ren-Sato-1212@users.noreply.github.com>
2023-07-21 00:40:28 +0900

    Revert "new_app_springboot"

--- source/kkn/lib/home/dto/home_response_dto.dart
+++
@@ -1,9 +0,0 @@
-import 'home_dto.dart';
-
-class HomeResponseDto {
-  String errorMessage;
-
-  HomeDto? homeDto;
-
-  HomeResponseDto(this.errorMessage);
-}
--- source/kkn_spring_boot_bonseong/src/main/java/com/kkn/www/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kkn.www.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kkn.www.entity.Member;
-
-public interface MemberRepository extends JpaRepository<Member, String>{
-	int countByUseridAndPassword(String userid, String password);
-}
