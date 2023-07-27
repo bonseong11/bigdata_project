c0aae8ee683fb4b32c44268356abe134f6cdd2ca
ren <131874337+Ren-Sato-1212@users.noreply.github.com>
2023-07-27 11:37:40 +0900

    MypageService.java modified

--- source/kkn_spring_boot/src/main/java/com/kkn/www/mypage/MypageService.java
+++ source/kkn_spring_boot/src/main/java/com/kkn/www/mypage/MypageService.java
@@ -1,9 +1,13 @@
 package com.kkn.www.mypage;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kkn.www.entity.HealthRecord;
 import com.kkn.www.entity.Member;
+import com.kkn.www.repository.HealthRecordRepository;
 import com.kkn.www.repository.MemberRepository;
 
 import jakarta.transaction.Transactional;
@@ -14,11 +18,20 @@ public class MypageService {
     @Autowired
     MemberRepository memberRepository;
     
+    @Autowired
+    HealthRecordRepository healthRecordRepository;
+    
     public MyPageDto mypageInformationLoadService(String userid) {
     	return MyPageDto.toMyPageDto(memberRepository.findById(userid).get());
     }
     
     public void mypageUpdateService(Member member) {
+    	memberUpdate(member);
+    	
+    	memberBmiUpdate(member);
+    }
+    
+    private void memberUpdate(Member member) {
     	Member oldMember = memberRepository.findById(member.getUserid()).get();
     	
     	if(member.getPassword() != null) {
@@ -34,4 +47,17 @@ public class MypageService {
         
         memberRepository.save(oldMember);
     }
+    
+    private void memberBmiUpdate(Member member) {
+    	HealthRecord oldMemberHealthRecord = healthRecordRepository.findByMemberUseridAndHealthdateBetween(member.getUserid(), LocalDate.now().toString(), LocalDate.now().plusDays(1).toString()).get(0);
+    	
+    	oldMemberHealthRecord.setBmi(this.bmiCalulate(member.getHeight(), member.getWeight()));
+    	
+    	oldMemberHealthRecord.setHealthdate(LocalDate.parse(oldMemberHealthRecord.getHealthdate().split(" ")[0]).toString());
+    	healthRecordRepository.save(oldMemberHealthRecord);
+    }
+    
+    private double bmiCalulate(double height, double weight) {
+		return weight / (height * height * 0.0001);
+	}
 }
