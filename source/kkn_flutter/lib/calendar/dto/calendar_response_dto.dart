b530d70e65099e5515cd4fb733c40b248bf01ae0
bonseong11 <80501715+bonseong11@users.noreply.github.com>
2023-08-01 10:04:26 +0900

    final

--- source/kkn_flutter/lib/calendar/dto/calendar_response_dto.dart
+++
@@ -1,9 +0,0 @@
-import 'package:kkn/calendar/dto/calendar_dto.dart';
-
-class CalendarResponseDto {
-  String errorMessage;
-
-  CalendarDto? calendarDto;
-
-  CalendarResponseDto(this.errorMessage);
-}
--- source/kkn_flutter/lib/home/dto/home_response_dto.dart
+++
@@ -1,9 +0,0 @@
-import 'package:kkn/home/dto/home_dto.dart';
-
-class HomeResponseDto {
-  String errorMessage;
-
-  HomeDto? homeDto;
-
-  HomeResponseDto(this.errorMessage);
-}
